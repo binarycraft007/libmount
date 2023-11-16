411c403dcf5fdbcc558142610e56a0ac0ecb47d1
binarycraft007 <elliot.huang.signed@gmail.com>
2023-11-16 17:37:41 +0800

    improve test

--- tests/main.zig
+++ tests/main.zig
@@ -3,6 +3,17 @@ const c = @cImport(@cInclude("libmount.h"));
 const testing = std.testing;
 
 test "basic add functionality" {
-    var tab = c.mnt_new_table_from_dir("mnt");
-    _ = tab;
+    var ctx = c.mnt_new_context();
+    try testing.expect(ctx != null);
+    defer c.mnt_free_context(ctx);
+    try std.fs.cwd().makeDir("mnt");
+    defer std.fs.cwd().deleteDir("mnt") catch unreachable;
+    var ret: c_int = 0;
+    ret = c.mnt_context_set_source(ctx, "tests/testdata/test.ext4");
+    try testing.expect(ret == 0);
+    ret = c.mnt_context_set_target(ctx, "tests/mnt");
+    try testing.expect(ret == 0);
+    if (c.mnt_context_mount(ctx) == 0) {
+        try testing.expect(c.mnt_context_umount(ctx) == 0);
+    }
 }
